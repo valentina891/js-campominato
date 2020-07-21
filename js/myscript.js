e5f544be94274ffaec39ea3c7bbfae52c16419e6
vale <valemil.vs@gmail.com>
2020-07-21 15:41:01 +0200

    Init

---
+++ js/myscript.js
@@ -0,0 +1,44 @@
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati
+
+var casuali = [];
+
+while (casuali.lenght < 16) {
+    var numero = generaRandom(1,100);
+    if (trovaElemento(casuali,numero) == false) {
+        casuali.push(numero);
+    }
+}
+console.log(casuali);
+
+// In seguito deve chiedere all'utente (100 - 16) volte di inserire un numero alla volta,
+// sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina,
+// altrimenti si continua chiedendo all'utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero
+// massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio,
+// cioè il numero di volte che l’utente ha inserito un numero consentito.
+// BONUS: (da fare solo se funziona tutto il resto)
+// all'inizio il software richiede anche una difficoltà all'utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 =>  tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
+
+
+// ****mie Funzioni****//
+function generaRandom(min,max) {
+    return Math.floor(Math.random()* (max - min + 1)) + min;
+}
+
+function trovaElemento(array,elemento) {
+    var i = 0;
+    while (i < array.lenght) {
+        if (elemento == array[i]) {
+            return true;
+        }
+    i++;
+    }
+    return false;
+}
